e30fcf84b4003153debe6d79ffedce70ae174aef
satch-mo <satchel0293@gmail.com>
2022-06-05 19:09:15 -0500

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = (props) => {
+  return (
+    <form onSubmit={props.handleRobotAdd}>
+      <p>What is your new robot's name?</p>
+      <input type="text" placeholder="name" onChange={props.handleInput} />
+      <button>Create Robot</button>
+    </form>
+  );
+};
+
+export default Form;
